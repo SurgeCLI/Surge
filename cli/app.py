38352c51b0dc20e7bc83298891384a9553f43b6a
sidsun1 <siddharthsundar2@gmail.com>
2025-08-22 19:12:58 -0700

    Working on top cmd and formatting other metrics

--- cli/app.py
+++ cli/app.py
@@ -5,6 +5,33 @@ from typing import Annotated
 
 app = typer.Typer(help = 'Surge - A DevOps CLI Tool For System Monitoring and Production Reliability')
 
+
+def run_cmd(cmd: str) -> str:
+    """
+    Helper function to abstract lengthy subprocess command implementation :D
+    """
+
+    return subprocess.run(cmd, shell=True, capture_output=True, text=True).stdout.strip()
+
+
+def get_load() -> tuple[float] | int:
+    """
+    Returns (averages, cores).
+    provider: 'linux' or 'psutil' (if psutil is not available, falls back to linux)
+    """
+
+    uptime = run_cmd("uptime | awk -F'average:' '{print $2}'")
+    averages = [float(x.replace(',', '')) for x in uptime.split()]
+    cores = run_cmd('nproc')
+
+    return averages, cores
+
+
+def get_cpu() -> tuple:
+    top = run_cmd('top -bn1')
+    return top
+
+
 @app.command()
 def monitor(
     load: Annotated[bool, typer.Option('-l', '--load', help = 'Show system load averages')] = True,
@@ -29,12 +56,11 @@ def monitor(
     if load:
         # Psutil substitute: averages = psutil.getloadavg(), cores = psutil.cpu_count()
 
-        uptime = subprocess.run("uptime | awk -F'average:' '{print $2}'", shell=True, capture_output=True, text=True).stdout.strip()
-        cores = int(subprocess.run('nproc', shell=True, capture_output=True, text=True).stdout.strip())
-        averages = [float(x.replace(',', '')) for x in uptime.split()]
+        averages, cores = get_load()
 
         print('System Load Averages')
         print('---------------------')
+
         if not verbose:
             print(f'Load avg (1m): {averages[0]}')
             print(f'Load avg (5m): {averages[1]}')
@@ -43,12 +69,22 @@ def monitor(
             print(f'Load avg (1m): {averages[0]:.2f} ({averages[0] / cores:.3f} per CPU)')
             print(f'Load avg (5m): {averages[1]:.2f} ({averages[1] / cores:.3f} per CPU)')
             print(f'Load avg (15m): {averages[2]:.2f} ({averages[2] / cores:.3f} per CPU)')
+    
+    if cpu:
+        cpu_util = get_cpu()
+
+        print('\nSystem CPU Utilization')
+        print('----------------------')
+    
+        if not verbose:
+            print(f'CPU: {cpu_util}')
 
 
     if verbose:
         print('Placeholder for verbose system metrics.')
     else:
         print('Placeholder for default system metrics')
+
 
 @app.command()
 def network(
